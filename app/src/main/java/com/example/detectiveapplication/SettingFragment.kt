56df9c74686ce9ad36455de4317711e1c88a5886
amroahmed109 <amro.ahmed109@gmail.com>
2022-03-16 21:37:20 +0200

    SettingFragment.kt added

---
+++ app/src/main/java/com/example/detectiveapplication/SettingFragment.kt
@@ -0,0 +1,28 @@
+package com.example.detectiveapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.detectiveapplication.databinding.FragmentSettingBinding
+
+
+class SettingFragment : Fragment() {
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+}
