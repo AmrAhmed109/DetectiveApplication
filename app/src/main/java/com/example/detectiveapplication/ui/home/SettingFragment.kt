4155f01008ea02ef246bf79ffb58ebe53379dd92
amroahmed109 <amro.ahmed109@gmail.com>
2022-03-26 20:09:19 +0200

    Update some designs

--- app/src/main/java/com/example/detectiveapplication/ui/home/SettingFragment.kt
+++ app/src/main/java/com/example/detectiveapplication/ui/home/SettingFragment.kt
@@ -1,16 +1,18 @@
-package com.example.detectiveapplication
+package com.example.detectiveapplication.ui.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
 import com.example.detectiveapplication.databinding.FragmentSettingBinding
 
 
 class SettingFragment : Fragment() {
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
