a536df2471a4460b2b596ba028c3c0a8aa0594a3
hassan shaban <61611892+hassan0shaban@users.noreply.github.com>
2022-04-01 16:13:11 +0200

    restructuring project

---
+++ app/src/main/java/com/example/detectiveapplication/DetectiveApplication.kt
@@ -0,0 +1,11 @@
+package com.example.detectiveapplication
+
+import android.app.Application
+
+class DetectiveApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
