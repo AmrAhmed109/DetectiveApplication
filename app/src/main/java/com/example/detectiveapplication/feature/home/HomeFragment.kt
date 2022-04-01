079b7da0ce5190faff4823ca33eafb40057ba943
hassan shaban <61611892+hassan0shaban@users.noreply.github.com>
2022-04-01 15:01:05 +0200

    restructuring project

--- app/src/main/java/com/example/detectiveapplication/feature/home/HomeFragment.kt
+++ app/src/main/java/com/example/detectiveapplication/feature/home/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.detectiveapplication.databinding.FragmentHomeBinding
-import com.example.detectiveapplication.ui.home.HomeFragmentDirections
 
 
 class HomeFragment : Fragment() {
